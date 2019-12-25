310b6a0b9e50b3a498c4ceca6115c8b0051c0c49
qifanwu31202 <qifanwu0413@gmail.com>
2019-12-25 09:10:59 +0000

    searchAPI

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"reflect"
+	"strconv"
+
+	"github.com/olivere/elastic"
+)
+
+const (
+	POST_INDEX = "post"
+	DISTANCE   = "2000km" //default search range
+	ES_URL     = "http://10.128.0.2:9200"
 )
 
 type Location struct {
@@ -12,6 +23,98 @@ type Location struct {
 	Lon float64 `json:"lon"`
 }
 
+type Post struct {
+	User     string   `json:"user"`
+	Message  string   `json:"message"`
+	Location Location `json:"location"`
+	Url      string   `json:"url"`
+	Type     string   `json:"type"`
+	Face     float32  `json:"face"`
+}
+
+func main() {
+	fmt.Println("started-service")
+	http.HandleFunc("/post", handlerPost)
+	http.HandleFunc("/search", handlerSearch)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	// Parse from body of request to get a json object.
+	fmt.Println("Received one post request")
+	decoder := json.NewDecoder(r.Body)
+	var p Post
+	if err := decoder.Decode(&p); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+}
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+	// range is optional
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
+	fmt.Println("range is ", ran)
+
+	query := elastic.NewGeoDistanceQuery("location")
+	query = query.Distance(ran).Lat(lat).Lon(lon)
+	searchResult, err := readFromES(query, POST_INDEX)
+	if err != nil {
+		http.Error(w, "Failed to read post from Elasticsearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to read post from Elasticsearch %v.\n", err)
+		return
+	}
+	posts := getPostFromSearchResult(searchResult)
+
+	js, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
+		return
+	}
+	w.Write(js)
+}
+
+func readFromES(query elastic.Query, index string) (*elastic.SearchResult, error) {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return nil, err
+	}
+
+	searchResult, err := client.Search().
+		Index(index).
+		Query(query).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return searchResult, nil
+}
+
+func getPostFromSearchResult(searchResult *elastic.SearchResult) []Post {
+	var ptype Post
+	var posts []Post
+
+	for _, item := range searchResult.Each(reflect.TypeOf(ptype)) {
+		if p, ok := item.(Post); ok {
+			posts = append(posts, p)
+		}
+	}
+	return posts
+}
+
+/***
 type Post struct {
 	User     string   `json:"user"`
 	Message  string   `json:"message"`
@@ -24,6 +127,7 @@ type Post struct {
 func main() {
 	fmt.Println("successfully start service")
 	http.HandleFunc("/post", handlerPost)
+	http.HandleFunc("/search", handlerSearch)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -36,5 +140,40 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 		//return
 	}
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)
-
 }
+
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+	// range is optional
+	fmt.Println("lat and lon:", lat, lon)
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
+	fmt.Println("range is ", ran)
+
+	query := elastic.NewGeoDistanceQuery("location")
+	query = query.Distance(ran).Lat(lat).Lon(lon)
+	searchResult, err := readFromES(query, POST_INDEX)
+	if err != nil {
+		http.Error(w, "Failed to read post from Elasticsearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to read post from Elasticsearch %v.\n", err)
+		return
+	}
+	posts := getPostFromSearchResult(searchResult)
+
+	js, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
+		return
+	}
+	w.Write(js)
+}
+***/
